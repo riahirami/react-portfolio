20818ff32cb0a6133396375d1f50adb2eddcc0dd
Rami Riahi <rriahi@anypli.com>
2024-06-09 01:52:45 +0100

    fix particles issues

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,10 @@
-import { RecursivePartial } from "react-tsparticles";
+import {} from "react-tsparticles";
 import { Project } from "../models/Project";
 import { images } from "./images";
+import { RecursivePartial, tsParticles } from "@tsparticles/engine";
+import { ParticlesOptions } from "models/ParticlesOptionsType";
+// import { RecursivePartial } from "@tsparticles/engine";
+
 export const TECHNOLOGIES_ICONS = [
   {
     src: images.bootstrapIcon,
@@ -128,173 +132,173 @@ export const TECHNOLOGIES_ICONS = [
     height: 20,
   },
 ];
-
-export const PARTICLES_OPTIONS: RecursivePartial<any> | undefined = {
-  fullScreen: {
-    enable: true,
-    zIndex: 1,
-  },
-  detectRetina: true,
-  fpsLimit: 60,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: "push",
-      },
-      onDiv: {
-        elementId: "repulse-div",
-        enable: false,
-        mode: "repulse",
-      },
-      onHover: {
-        enable: true,
-        mode: "bubble",
-        parallax: {
-          enable: false,
-          force: 60,
-          smooth: 10,
-        },
-      },
-      resize: true,
-    },
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 2,
-      },
-      connect: {
-        distance: 80,
-        lineLinked: {
-          opacity: 0.5,
-        },
-        radius: 60,
-      },
-      grab: {
-        distance: 400,
-        lineLinked: {
-          opacity: 1,
-        },
-      },
-      push: {
-        quantity: 2,
-      },
-      remove: {
-        quantity: 2,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: "#ffffff",
-    },
-    lineLinked: {
-      blink: false,
-      color: "#000",
-      consent: false,
-      distance: 150,
-      enable: false,
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      attract: {
-        enable: false,
-        rotate: {
-          x: 600,
-          y: 1200,
-        },
-      },
-      bounce: false,
-      direction: "none",
+export const PARTICLES_OPTIONS: RecursivePartial<ParticlesOptions> | undefined =
+  {
+    fullScreen: {
       enable: true,
-      outMode: "out",
-      random: false,
-      speed: 2,
-      straight: false,
+      zIndex: 1,
     },
-    number: {
-      density: {
-        enable: true,
-        area: 800,
+    detectRetina: true,
+    fpsLimit: 60,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onDiv: {
+          elementId: "repulse-div",
+          enable: false,
+          mode: "repulse",
+        },
+        onHover: {
+          enable: true,
+          mode: "bubble",
+          parallax: {
+            enable: false,
+            force: 60,
+            smooth: 10,
+          },
+        },
+        resize: true,
       },
-      limit: 20,
-      value: 15,
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 2,
+        },
+        connect: {
+          distance: 80,
+          lineLinked: {
+            opacity: 0.5,
+          },
+          radius: 60,
+        },
+        grab: {
+          distance: 400,
+          lineLinked: {
+            opacity: 1,
+          },
+        },
+        push: {
+          quantity: 2,
+        },
+        remove: {
+          quantity: 2,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
     },
-    opacity: {
-      animation: {
-        enable: true,
-        minimumValue: 0.2,
-        speed: 1,
-        sync: false,
+    particles: {
+      color: {
+        value: "#ffffff",
       },
-      random: true,
-      value: 1,
-    },
-    rotate: {
-      animation: {
-        enable: true,
-        speed: 5,
-        sync: false,
-      },
-      direction: "random",
-      random: true,
-      value: 0,
-    },
-    shape: {
-      character: {
-        fill: false,
-        font: "Verdana",
-        style: "",
-        value: "*",
-        weight: "400",
-      },
-      image: TECHNOLOGIES_ICONS,
-      polygon: {
-        sides: 5,
-      },
-      stroke: {
-        color: "#000000",
-        width: 0,
-      },
-      type: "image",
-    },
-    size: {
-      animation: {
+      lineLinked: {
+        blink: false,
+        color: "#000",
+        consent: false,
+        distance: 150,
         enable: false,
-        minimumValue: 0.1,
-        speed: 40,
-        sync: false,
+        opacity: 0.4,
+        width: 1,
       },
-      random: false,
-      value: 16,
+      move: {
+        attract: {
+          enable: false,
+          rotate: {
+            x: 600,
+            y: 1200,
+          },
+        },
+        bounce: false,
+        direction: "none",
+        enable: true,
+        outMode: "out",
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        limit: 20,
+        value: 15,
+      },
+      opacity: {
+        animation: {
+          enable: true,
+          minimumValue: 0.2,
+          speed: 1,
+          sync: false,
+        },
+        random: true,
+        value: 1,
+      },
+      rotate: {
+        animation: {
+          enable: true,
+          speed: 5,
+          sync: false,
+        },
+        direction: "random",
+        random: true,
+        value: 0,
+      },
+      shape: {
+        character: {
+          fill: false,
+          font: "Verdana",
+          style: "",
+          value: "*",
+          weight: "400",
+        },
+        image: TECHNOLOGIES_ICONS,
+        polygon: {
+          sides: 5,
+        },
+        stroke: {
+          color: "#000000",
+          width: 0,
+        },
+        type: "image",
+      },
+      size: {
+        animation: {
+          enable: false,
+          minimumValue: 0.1,
+          speed: 40,
+          sync: false,
+        },
+        random: false,
+        value: 16,
+      },
     },
-  },
-  polygon: {
-    draw: {
-      enable: false,
-      lineColor: "#ffffff",
-      lineWidth: 0.5,
+    polygon: {
+      draw: {
+        enable: false,
+        lineColor: "#ffffff",
+        lineWidth: 0.5,
+      },
+      move: {
+        radius: 10,
+      },
+      scale: 1,
+      url: "",
     },
-    move: {
-      radius: 10,
+    background: {
+      image: "",
+      position: "50% 50%",
+      repeat: "no-repeat",
+      size: "cover",
     },
-    scale: 1,
-    url: "",
-  },
-  background: {
-    image: "",
-    position: "50% 50%",
-    repeat: "no-repeat",
-    size: "cover",
-  },
-};
+  };
 
 export const FIRST_NAME = "Rami";
 export const LAST_NAME = "Riahi";
